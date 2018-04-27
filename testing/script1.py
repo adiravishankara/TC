cee049fbcbd1d456c9223789b8629e8c71d7b915
Adithya Ravishankara <arka_999@hotmail.com>
2018-04-27 11:27:06 -0700

    still testing

--- testing/script1.py
+++ testing/script1.py
@@ -4,12 +4,11 @@ import random
 from time import sleep
 
 
-while True:
-	rand1 = random.randint(1,100)
-	time1 = time.time()
-	stringa = (time1, "random value: ", rand1)
-	print(stringa)
-	printstat = True
-	sleep(2)
-	printstat = False
+rand1 = random.randint(1,100)
+time1 = time.time()
+stringa = (time1, "random value: ", rand1)
+print(stringa)
+printstat = True
+sleep(2)
+printstat = False
 

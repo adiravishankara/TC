73bf605999cd1f6c1e38fae3fe355b3130988850
Adithya Ravishankara <arka_999@hotmail.com>
2018-04-27 11:20:49 -0700

    Added a testing file

---
+++ testing/script1.py
@@ -0,0 +1,15 @@
+import time
+import subprocess, sys
+import random
+from time import sleep
+
+
+while True:
+	rand1 = random.randint(1,100)
+	time1 = time.time()
+	stringa = (time1, "random value: ", rand1)
+	print(stringa)
+	printstat = True
+	sleep(2)
+	printstat = False
+
---
+++ testing/script2.py
@@ -0,0 +1,7 @@
+from script1.py import stringa, printstat, rand1
+
+while True:
+	print(stringa)
+	if(printstat == True):
+		print(rand1-10)
+

3eb2bef82aad13e5b848b795ff5ea87a7637696f
Adithya Ravishankara <arka_999@hotmail.com>
2018-04-30 12:49:25 -0700

    The new data file

---
+++ DATA/startUp.js
@@ -0,0 +1,22 @@
+rcb.files.newLogFile({prefix:"TC"});
+
+function readSensor(){
+    rcb.console.setVerbose(false);
+    rcb.sensors.read(readDone, 10);
+    rcb.console.setVerbose(true);
+}
+function readDone(result){
+    rcb.console.setVerbose(false);
+    rcb.files.newLogEntry(result,readSensor);
+    rcb.console.setVerbose(true);
+}
+function send_start(){
+    rcb.output.pwm("esc", 1000);
+    rcb.wait(function(){},1);
+    rcb.output.pwm("esc", 1000);
+    rcb.wait(function(){},1);
+    rcb.output.pwm("esc",1000);
+    rcb.wait(function(){},1);
+    rcb.output.pwm("esc",1500);
+    rcb.wait(readSensor(),0.00000000001);
+}

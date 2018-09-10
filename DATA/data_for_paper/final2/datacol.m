de7a5dbd5cc64d27e508908720a7bcf04eea99b0
Adithya Ravishankara <arka_999@hotmail.com>
2018-09-09 22:21:50 -0700

    Created seperate workspaces for each of the different tests

--- DATA/data_for_paper/final2/datacol.m
+++ DATA/data_for_paper/final2/datacol.m
@@ -1,8 +1,7 @@
-pt= csvread("../final3/thr2pwm.csv",1,0);
+prop_faultT= csvread("../final3/paper_data/prop_fault.csv",1,0);
 %  curr_testtorq = pt;
-[r,c] = find(pt(:,10) == 0);
-
-pt(r,:) = [];
+[r,c] = find(prop_faultT(:,10) == 0);
+prop_faultT(r,:) = [];
 
 % [r,c] = find(curr_testtorq(:,9) == 0);
 % 
